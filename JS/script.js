8dede4655dbd77cdedaf3e757618e10e61525cfa
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-02-09 17:51:28 +0530

    UI overhaul: navbar, hero section, styling and form JS

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,16 @@
 const form =document.getElementById("BookingForm")
+
+
 form.addEventListener("submit", function (event){
     event.preventDefault();
-    console.log("Form submitted");
+    const name =document.getElementById("name").value
+    const email =document.getElementById("gmail").value
+    if(name === ''|| email ===''){
+        alert("please fill all required field")
+        return;
+    }
+    
+    alert("Booking request submitted successfully!")
+    console.log("Form submitted",name,email);
+    form.reset()
 })
