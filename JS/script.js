de49d0955b31d795c74f919f75ba4f91f4365b4e
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-04-03 14:15:40 +0530

    handle more form problem

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,18 @@ const form =document.getElementById("BookingForm")
 
 form.addEventListener("submit", function (event){
     event.preventDefault();
-    const name =document.getElementById("name").value
-    const email =document.getElementById("gmail").value
+    const name =document.getElementById("name").value.trim()
+    const email =document.getElementById("gmail").value.trim()
     if(name === ''|| email ===''){
         alert("please fill all required field")
+        return;
+    }
+    else if(!email.endsWith('@gmail.com')){
+        alert("please enter correct gmail")
+        return;
+    }
+    else if(name ===' '){
+         alert("please fill all required field")
         return;
     }
     
