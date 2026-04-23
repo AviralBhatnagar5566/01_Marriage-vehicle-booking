68bb7c465bad28430f9bdc305c6da2e9786cae0e
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-04-23 15:52:57 +0530

    handle more form problem

--- JS/script.js
+++ JS/script.js
@@ -5,20 +5,43 @@ form.addEventListener("submit", function (event){
     event.preventDefault();
     const name =document.getElementById("name").value.trim()
     const email =document.getElementById("gmail").value.trim()
-    if(name === ''|| email ===''){
-        alert("please fill all required field")
-        return;
+    const date = document.getElementById("date").value.trim()
+    const selectedDate = new Date(date)
+    const today = new Date()
+    today.setHours(0,0,0,0)
+    let isvalue = true
+     // Clear old errors
+    document.getElementById("nameError").innerText = "";
+    document.getElementById("gmailError").innerText = "";
+    document.getElementById("dateError").innerText = "";
+    document.getElementById("sucessMsg").innerText = "";
+
+    
+
+    if(name === ''){
+        document.getElementById("nameError").innerText = "🚗Write a valid name"
+        isvalue = false
     }
-    else if(!email.endsWith('@gmail.com')){
-        alert("please enter correct gmail")
-        return;
+    if (!email.endsWith('@gmail.com')){
+        document.getElementById("gmailError").innerText = "🚗 Write a valid Email"
+        isvalue = false
     }
-    else if(name ===' '){
-         alert("please fill all required field")
-        return;
+    if(!date){
+        document.getElementById("dateError").innerText = "select Date";
+    }
+
+    if (selectedDate < today) {
+        document.getElementById("dateError").innerText = "🗓️ Date cannot be in the past";
+        isvalue = false
+    }
+    if(isvalue){
+        document.getElementById("sucessMsg").innerText ="✅ Booking submitted successfully!"
+       
+        form.reset()
+        
+        
+        console.log("Form submitted",name,email);
+    
     }
     
-    alert("Booking request submitted successfully!")
-    console.log("Form submitted",name,email);
-    form.reset()
 })
