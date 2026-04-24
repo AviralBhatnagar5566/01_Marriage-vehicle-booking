c2b213dbf09cb1afefec366de6075193bc7ad95a
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-04-24 14:22:54 +0530

    api handling

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
 const form =document.getElementById("BookingForm")
 
 
-form.addEventListener("submit", function (event){
+form.addEventListener("submit",async function (event){
     event.preventDefault();
     const name =document.getElementById("name").value.trim()
     const email =document.getElementById("gmail").value.trim()
     const date = document.getElementById("date").value.trim()
+    const Vehicle = document.getElementById("Vehicle").value.trim()
     const selectedDate = new Date(date)
     const today = new Date()
     today.setHours(0,0,0,0)
@@ -34,14 +35,52 @@ form.addEventListener("submit", function (event){
         document.getElementById("dateError").innerText = "🗓️ Date cannot be in the past";
         isvalue = false
     }
+    if(!Vehicle){
+        document.getElementById("VehicleError").innerText ="#️⃣ fill the number of Vehicle"
+        isvalue = false
+    }
+    if(Vehicle > 10){
+        document.getElementById("VehicleError").innerText ="⚠️Cannot Book More Than 10 Vehicle"
+        isvalue = false
+    }
     if(isvalue){
-        document.getElementById("sucessMsg").innerText ="✅ Booking submitted successfully!"
+        const success = await api(name,email)
+        if(success){
+            document.getElementById("sucessMsg").innerText ="✅ Booking submitted successfully!"
+            form.reset()
+        }
+       else{
+            document.getElementById("sucessMsg").innerText ="❌ SomeThing Went Wrong"
+
+       }
        
-        form.reset()
         
         
-        console.log("Form submitted",name,email);
+        
+        console.log("Form submitted",name,email,date);
     
     }
     
+
+    async function api(name,gmail,date) {
+        try{
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+               method: "POST",
+               headers:{"Content-Type": "application/json"},
+                body: JSON.stringify({
+                name,
+                email,
+                date
+             })
+            })
+            const data = await response.json();
+            console.log(data);
+            
+            return true
+        }
+        catch(error){
+            console.log("error",error);
+            return false
+        }
+    }
 })
